33f8a010a1718ff1e2ba81a2bc646c8417d3f93d
fathimaca369 <fathimacafathimaca152@gmail.com>
2025-09-17 16:06:47 +0530

    Add files via upload

---
+++ rect.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int l,b,a,p;
+print("enter the length and breadth of rectangle:");
+scanf("%d%,&l,&b);
+a=l*b;
+p=2*(l+b);
+printf("area and perimeter of rectangle is %d %d",a,p);
+}
